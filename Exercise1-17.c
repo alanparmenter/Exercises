beae22cc4809eb0f901aeb5d691818c689e799f9
Alan Parmenter <alanparmenter@gmail.com>
2022-08-27 20:32:58 +0100

    Update Exercises1-17.c
    Solution which prints lines as they come

--- Exercise1-17.c
+++ Exercise1-17.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #define MAXLINE 4096  /* maximum input line size*/
+#define MINLINE 80    /* minimum size to print line*/
 
 int gitline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print longest input line */
-main()
+int main()
 {
-  int len;            /* current line length */
-  int max;            /* maximum line length seen so far */
-  char line[MAXLINE];       /* current input line */
-  char longest[MAXLINE];    /* longest line saved here */
+  int len;               /* current line length */
+  char longer[MAXLINE];  /* lines longer than MINLINE*/
+  char line[MAXLINE];    /* current input line */
 
-  max = 0;
   while ((len = gitline(line, MAXLINE)) > 0)
-    if (len > max) {
-      max = len;
-      copy(longest, line);
+    if (len > MINLINE + 1) {
+      copy(longer, line);
+      printf("%s\n", longer);
     }
-  if (max > 0)    /* there was a line */
-    printf("%s\n", longest);
-    printf("Length %d\n", max-1);
   return 0;
 }
 
 /* gitline: read a line into s, return length */
 int gitline(char s[], int lim)
 {
-  int c, i;
+  int c = 0, i;
 
   for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
     s[i] = c;
